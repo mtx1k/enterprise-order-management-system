b005faf690e54ec3f94df213902af680b1d74a22
Herr Oleh Bryl <olegbryl@gmail.com>
2025-01-27 15:04:36 +0100

    add OrderStatus

--- src/main/java/com/final_project/ua_team_final_project/models/OrderStatus.java
+++ src/main/java/com/final_project/ua_team_final_project/models/OrderStatus.java
@@ -3,6 +3,8 @@ package com.final_project.ua_team_final_project.models;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name="order_status")
 public class OrderStatus {
@@ -12,4 +14,46 @@ public class OrderStatus {
 
     private String statusText;
 
+    public OrderStatus() {
+    }
+
+    public OrderStatus(Long statusId, String statusText) {
+        this.statusId = statusId;
+        this.statusText = statusText;
+    }
+
+    public Long getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getStatusText() {
+        return statusText;
+    }
+
+    public void setStatusText(String statusText) {
+        this.statusText = statusText;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof OrderStatus that)) return false;
+        return Objects.equals(statusId, that.statusId) && Objects.equals(statusText, that.statusText);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(statusId, statusText);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderStatus{" +
+                "statusId=" + statusId +
+                ", statusText='" + statusText + '\'' +
+                '}';
+    }
 }
