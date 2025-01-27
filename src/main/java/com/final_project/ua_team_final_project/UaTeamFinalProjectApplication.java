4d366d968a68ccb8ffc13b09cc97f1a464a67880
Herr Oleh Bryl <olegbryl@gmail.com>
2025-01-27 11:11:48 +0100

    initaial commit

---
+++ src/main/java/com/final_project/ua_team_final_project/UaTeamFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.final_project.ua_team_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaTeamFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaTeamFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/final_project/ua_team_final_project/UaTeamFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_project.ua_team_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaTeamFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
