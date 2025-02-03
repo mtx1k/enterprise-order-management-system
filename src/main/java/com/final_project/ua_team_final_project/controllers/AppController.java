918c1a80ecc28108200cc89c57136135d5f80a6e
Alex <oleksandr.ovcharenko@dci-student.org>
2025-02-03 14:11:06 +0100

    Initial commit

--- src/main/java/com/final_project/ua_team_final_project/controllers/AppController.java
+++ src/main/java/com/final_project/ua_team_final_project/controllers/AppController.java
@@ -3,32 +3,17 @@ package com.final_project.ua_team_final_project.controllers;
 
 import com.final_project.ua_team_final_project.models.*;
 import com.final_project.ua_team_final_project.repositories.*;
-
-import com.final_project.ua_team_final_project.models.Role;
 import com.final_project.ua_team_final_project.models.User;
 import com.final_project.ua_team_final_project.repositories.DepartmentRepository;
 import com.final_project.ua_team_final_project.repositories.RoleRepository;
 import com.final_project.ua_team_final_project.repositories.UserRepository;
-
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import com.final_project.ua_team_final_project.services.PageDataManager;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-
-import javax.sql.DataSource;
-import java.util.ArrayList;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 import java.security.Principal;
-
 import java.util.List;
 import java.util.Map;
 
