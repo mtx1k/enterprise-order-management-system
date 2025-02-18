98d611ddec872f8da2257ddb2c36c430e099d815
Herr Oleh Bryl <olegbryl@gmail.com>
2025-02-18 11:15:48 +0100

    teble.css + color for checkbox

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.querySelector('.logo-left').classList.add('show-logo');
         document.querySelector('.logo-right').classList.add('show-logo');
-    }, 2000); // Задержка в 2 секунды
+    }, 2000);
 });
