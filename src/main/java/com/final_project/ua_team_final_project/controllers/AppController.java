3eb8e90ac1f7250e0ae5383e605bf54d957c0448
Alex <oleksandr.ovcharenko@dci-student.org>
2025-02-19 14:35:11 +0100

    feels like it's working

--- src/main/java/com/final_project/ua_team_final_project/controllers/AppController.java
+++ src/main/java/com/final_project/ua_team_final_project/controllers/AppController.java
@@ -129,16 +129,6 @@ public class AppController {
         return "/organization/editProducts";
     }
 
-//    @GetMapping("/editProducts")
-//    public String editProducts(Model model) {
-//        OrderedProduct orderedProduct = new OrderedProduct();
-//        orderedProduct.setItemPrice(orderedProduct.getItemPrice());
-//        orderedProduct.setAmount(orderedProduct.getAmount());
-//        model.addAttribute("orderedProduct", orderedProduct);
-//
-//        return "organization/editProducts";
-//    }
-
     @PostMapping("/confirmOrder")
     public String confirmOrder(@RequestParam List<Long> selectedProducts,
                                @RequestParam List<Long> quantities) {
