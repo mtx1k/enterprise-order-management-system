3c152e7baa2bda465a58473ee146ef98482dfbff
Herr Oleh Bryl <olegbryl@gmail.com>
2025-02-19 15:35:13 +0100

    only Alex didn't give me info and photo

--- src/main/resources/static/js/teem.js
+++ src/main/resources/static/js/teem.js
@@ -33,3 +33,39 @@ document.querySelector('.close-modal').addEventListener('click', function() {
         modal.style.display = 'none';
     }, 1500);
 });
+
+document.getElementById('shiftButton2').addEventListener('click', function() {
+    const col2 = document.querySelector('.col2');
+    const modal2 = document.getElementById('modal2');
+    const modalContent2 = document.querySelector('.modal-content2');
+
+    col2.style.transform = 'translateX(-600px)';
+
+    modal2.style.display = 'block';
+    setTimeout(() => {
+        modal2.style.transform = 'translateX(300px)';
+        modal2.style.opacity = '1';
+        modalContent2.style.transform = 'translateX(0)';
+        modalContent2.style.opacity = '1';
+    }, 10);
+});
+
+document.querySelector('.close-modal2').addEventListener('click', function() {
+    const col2 = document.querySelector('.col2');
+    const modal2 = document.getElementById('modal2');
+    const modalContent2 = document.querySelector('.modal-content2');
+
+
+    col2.style.transform = 'translateX(0)';
+
+
+    modal2.style.transform = 'translateX(-50%)';
+    modal2.style.opacity = '0';
+    modalContent2.style.transform = 'translateX(100%)';
+    modalContent2.style.opacity = '0';
+
+
+    setTimeout(() => {
+        modal2.style.display = 'none';
+    }, 1500);
+});
